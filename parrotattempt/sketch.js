f38ff9f4c6d772a6ca9d953e76ab856ccffaaada
Felipe Trost <felipe.trost@gmail.com>
2020-07-12 00:33:48 -0400

    Added gradient

--- parrotattempt/sketch.js
+++ parrotattempt/sketch.js
@@ -1,10 +1,17 @@
 let birds = [];
+let c1, c2;
 
 function setup() {
     createCanvas(500, 500);
     background(55);
+
     // Degrees beacause why not
     angleMode(DEGREES);
+
+    // Cool colors for the gradient
+    const dark = .8;
+    c1 = color(240 * dark, 99 * dark, 164 * dark)
+    c2 = color(45 * dark, 197 * dark, 244 * dark)
 }
 
 // Add bird new birds when the mouse is clicked
@@ -13,13 +20,13 @@ function mousePressed() {
         new Bird(
             random(width),
             random(height),
-            random(.8, 1.3)
+            random(.8, 1.5)
         )
     )
 }
 
 function draw() {
-    background(55)
+    setGradient(0, 0, width, height, c1, c2);
 
     birds.forEach(bird => {
         bird.chase(createVector(mouseX, mouseY))
@@ -28,4 +35,17 @@ function draw() {
         bird.update();
         bird.draw();
     })
+}
+
+// I borrowed this from the p5js reference
+function setGradient(x, y, w, h, c1, c2, ) {
+    noFill();
+
+    // Top to bottom gradient
+    for (let i = y; i <= y + h; i++) {
+        let inter = map(i, y, y + h, 0, 1);
+        let c = lerpColor(c1, c2, inter);
+        stroke(c);
+        line(x, i, x + w, i);
+    }
 }
